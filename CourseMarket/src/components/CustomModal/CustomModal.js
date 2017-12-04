17f0915101822a6520cd6db77e2a164015d49ce2
Ferenc Péli <ferenc.peli@outlook.com>
2017-12-04 22:00:33 +0100

    Modal margin top fix

--- CourseMarket/src/components/CustomModal/CustomModal.js
+++ CourseMarket/src/components/CustomModal/CustomModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import './CustomModal.css';
 
 export default class CustomModal extends Component {
     constructor(props) {
